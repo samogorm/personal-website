34c696469e1d370c2602ac9cf668e6984773cb4c
Sam O'Gorman <samjogorman93@gmail.com>
2023-02-19 18:49:19 +0000

    Setup absolute imports/paths

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Sam O\'Gorman';
+export const SITE_DESCRIPTION = 'A Software Engineer focused on building apps with TypeScript.';
