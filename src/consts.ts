ea8b15ebc70c3bfce69545d0b5919e19ce703824
Sam O'Gorman <samjogorman93@gmail.com>
2023-03-20 19:17:49 +0000

    Remove unused files from Astro installation

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
 export const SITE_TITLE = 'Sam O\'Gorman';
-export const SITE_DESCRIPTION = 'A Software Engineer focused on building apps with TypeScript.';
+export const SITE_DESCRIPTION = 'A Software Engineer focussed on building apps with TypeScript.';
