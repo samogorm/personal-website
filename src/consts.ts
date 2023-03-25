82d85536040b7b7b19259e28ef60efed6b22019c
Sam O'Gorman <samjogorman93@gmail.com>
2023-03-25 17:07:29 +0000

    Move static content to the `consts` file
    Since I don't want to have to manage the content
    in a CMS, I've opted to pop it into a file to
    keep it all in one place.

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,36 @@
 export const SITE_TITLE = 'Sam O\'Gorman';
+export const SITE_SUBTITLE = 'Software Engineer';
 export const SITE_DESCRIPTION = 'A Software Engineer focussed on building apps with TypeScript.';
+export const EMAIL = 'samjogorman93@gmail.com';
+
+export const HOMEPAGE_CONTENT = {
+  title: 'Hey, I\'m a Software Engineer',
+  text: [
+    'Since 2016, I\'ve gained experience across most aspects of the application lifecycle, including frontend, backend, and agile ways of working.',
+    'I have a passion for observability and building resilient, modular and scaleable software.',
+    'When I\'m not at my computer, you\'ll find me walking or being bossed around by my Beagles.'
+  ]
+}
+
+export const SOCIAL_MEDIA_ITEMS = [
+  {
+    iconName: 'github',
+    description: 'Github',
+    href: 'https://github.com/samogorm',
+  },
+  {
+    iconName: 'instagram',
+    description: 'Instagram',
+    href: 'https://www.instagram.com/sam.ogorm',
+  },
+  {
+    iconName: 'linkedin-in',
+    description: 'Linked In',
+    href: 'https://www.linkedin.com/in/samantha-o-gorman-8b955284',
+  },
+  {
+    iconName: 'paper-plane',
+    description: 'Email me',
+    href: `mailto:${EMAIL}`,
+  },
+];
